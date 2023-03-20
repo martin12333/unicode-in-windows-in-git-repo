630580f94a2ab7b8b7b2c9af0409862528d56383
Martin Milan <martin12333@users.noreply.github.com>
2023-03-20 08:25:27 +0100

    smile-in-fnam

--- unicode-in-windows-in-git-repo.f8.sh
+++ unicode-in-windows-in-git-repo.f8.sh
@@ -1,6 +1,16 @@
 
 exit
 
+
+
+https://maryrosecook.com/blog/post/git-from-the-inside-out
+
+
+
+
+
+
+
 git clone https://github.com/martin12333/unicode-in-windows-in-git-repo.git
 
 code unicode-in-windows-in-git-repo
@@ -13,6 +23,20 @@ git push
 	git commit -v -a -m "smile-in-commit-msg☻"
 	git commit -v -a -m "smile-in-commit-msg@☺@thru-pwsh"
 
+code "C:\Users\marti\OneDrive\unicode-in-windows-in-git-repo\.git\COMMIT_EDITMSG"
+
+code "C:\Users\marti\OneDrive\unicode-in-windows-in-git-repo\.git\logs\HEAD"
+
+code "C:\Users\marti\OneDrive\unicode-in-windows-in-git-repo\.git\logs\refs\heads\main"
 
 
+
+mkdir data
+echo  'a' > data/letter.txt
+echo  'a' >"smile-in-fname@☺@"
+git add "smile-in-fname@☺@"
+	git commit -v -a -m "smile-in-fnam"
+
+
+git add data/letter.txt
 
