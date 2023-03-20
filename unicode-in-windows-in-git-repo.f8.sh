36cc2198bf28fa4aeb334ea4147602729daeddd9
Martin Milan <martin12333@users.noreply.github.com>
2023-03-20 07:56:57 +0100

    ☀ add

---
+++ unicode-in-windows-in-git-repo.f8.sh
@@ -0,0 +1,11 @@
+
+exit
+
+git clone https://github.com/martin12333/unicode-in-windows-in-git-repo.git
+
+code unicode-in-windows-in-git-repo
+
+git add C:\Users\marti\OneDrive\unicode-in-windows-in-git-repo\unicode-in-windows-in-git-repo.f8.sh
+
+	git commit -v -a -m "☀ add"
+
